fee55aae34b27d5aa565bbfe5874086fe5823ea2
Vitalii <seminenkovs@gmail.com>
2023-07-17 23:22:27 +0200

    git push

---
+++ Models/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider service)
+        {
+            using (var context = new LibraryContext(
+                
+                service.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Book.Any())
+                {
+                    return;
+                }
+
+                context.Book.AddRange(
+                    new Book { Title = "Tiny C4 book", CallNumber = "Axd 38947" },
+                    new Book { Title = "Blazor book", CallNumber = "df H454"},
+                    new Book { Title = "Fire Bird", CallNumber = " ABC 37"},
+                    new Book { Title = "Fairy tail", CallNumber = " pio 373737"}
+                );
+
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
